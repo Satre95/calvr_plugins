642c7345063f5935b79e98ffd4288230f7975be1
Siddharth Atre <sidatre@gmail.com>
2018-04-25 12:40:16 -0700

    Created Skeleton for StarForgePlugin class

--- satre/Star-Forge/StarForgePlugin.cpp
+++ satre/Star-Forge/StarForgePlugin.cpp
@@ -1,1 +1,25 @@
 #include "StarForgePlugin.hpp"
+
+StarForgePlugin::StarForgePlugin() {
+
+}
+
+StarForgePlugin::~StarForgePlugin() {
+
+}
+
+bool StarForgePlugin::init() {
+
+}
+
+void StarForgePlugin::preFrame() {
+
+}
+
+void StarForgePlugin::postFrame() {
+
+}
+
+bool StarForgePlugin::processEvent(cvr::InteractionEvent * event) {
+
+}
--- satre/Star-Forge/StarForgePlugin.hpp
+++ satre/Star-Forge/StarForgePlugin.hpp
@@ -1,8 +1,18 @@
 #pragma once
 
 #include <cvrKernel/CVRPlugin.h>
+#include <cvrKernel/InteractionEvent.h>
 #include <glm/glm.hpp>
 
 class StarForgePlugin: public cvr::CVRPlugin {
+public:
+
+	StarForgePlugin();
+	~StarForgePlugin();
 	
+	virtual bool init() override;
+	virtual void preFrame() override;
+	virtual void postFrame() override;
+	virtual bool processEvent(cvr::InteractionEvent * event) override;
+	virtual int getPriority() override { return 50; }
 };
